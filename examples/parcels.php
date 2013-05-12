2f85957f63fc86dac155251ce331c1eafddcbfe8
MDrollette <matt@drollette.com>
2013-05-11 20:22:28 -0500

    update examples

--- examples/parcels.php
+++ examples/parcels.php
@@ -1,33 +1,32 @@
 <?php
 
-require_once("../lib/easypost.php");
-
-EasyPost::setApiKey('cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi');
+require_once("../vendor/autoload.php");
+\EasyPost\EasyPost::setApiKey('cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi');
 
 // Parcel: create
 $params = array("length" => 20.2,
-    "width" => 10.9,
-    "height" => 5,
-    //"predefined_package" => null,
-    "weight" => 14.8
+                "width"  => 10.9,
+                "height" => 5,
+                //"predefined_package" => null,
+                "weight" => 14.8
 );
-$parcel = EasyPost_Parcel::create($params);
+$parcel = \EasyPost\Parcel::create($params);
 
 $params = array("predefined_package" => 'SmallFlatRateBox',
-    "weight" => 38.1
+                "weight"             => 38.1
 );
-$parcel = EasyPost_Parcel::create($params);
+$parcel = \EasyPost\Parcel::create($params);
 
 // retrieve
-$retrieved = EasyPost_Parcel::retrieve($parcel->id);
+$retrieved = \EasyPost\Parcel::retrieve($parcel->id);
 print_r($retrieved);
 
 // all
-$all = EasyPost_Parcel::all();
+$all = \EasyPost\Parcel::all();
 //print_r($all);
 /*
 for($i = 0, $k = count($all); $i < $k; $i++) {
-  print_r(EasyPost_Parcel::retrieve($all[$i]->id));
+  print_r(\EasyPost\Parcel::retrieve($all[$i]->id));
 }
 */
 
