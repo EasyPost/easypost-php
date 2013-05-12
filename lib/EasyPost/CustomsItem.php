2e9140fe1b02d0be81d2482c043f3792ac07c66c
MDrollette <matt@drollette.com>
2013-05-11 20:13:18 -0500

    formatted for PSR-2

--- lib/EasyPost/CustomsItem.php
+++ lib/EasyPost/CustomsItem.php
@@ -1,33 +1,46 @@
 <?php
 
-class EasyPost_CustomsItem extends EasyPost_Resource {
-  public static function constructFrom($values, $class=null, $apiKey=null) {
-    $class = get_class();
-    return self::constructFrom($values, $class, $apiKey);
-  }
+namespace EasyPost;
 
-  public static function retrieve($id, $apiKey=null) {
-    $class = get_class();
-    return self::_retrieve($class, $id, $apiKey);
-  }
+class CustomsItem extends Resource
+{
+    public static function constructFrom($values, $class = null, $apiKey = null)
+    {
+        $class = get_class();
 
-  public static function all($params=null, $apiKey=null) {
-    $class = get_class();
-    return self::_all($class, $params, $apiKey);
-  }
-
-  public static function create($params=null, $apiKey=null) {
-    $class = get_class();
-    if(!isset($params['customs_item']) || !is_array($params['customs_item'])) {
-      $clone = $params;
-      unset($params);
-      $params['customs_item'] = $clone;
+        return self::constructFrom($values, $class, $apiKey);
     }
-    return self::_create($class, $params, $apiKey);
-  }
-    
-  public function save() {
-    $class = get_class();
-    return self::_save($class);
-  }
+
+    public static function retrieve($id, $apiKey = null)
+    {
+        $class = get_class();
+
+        return self::_retrieve($class, $id, $apiKey);
+    }
+
+    public static function all($params = null, $apiKey = null)
+    {
+        $class = get_class();
+
+        return self::_all($class, $params, $apiKey);
+    }
+
+    public static function create($params = null, $apiKey = null)
+    {
+        $class = get_class();
+        if (!isset($params['customs_item']) || !is_array($params['customs_item'])) {
+            $clone = $params;
+            unset($params);
+            $params['customs_item'] = $clone;
+        }
+
+        return self::_create($class, $params, $apiKey);
+    }
+
+    public function save()
+    {
+        $class = get_class();
+
+        return self::_save($class);
+    }
 }
