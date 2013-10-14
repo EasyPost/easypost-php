a0b79ff698ac7b2faa736d8ba3d09fd32be9c47a
Michael Beale <michael.beale@gmail.com>
2013-10-14 14:09:10 -0600

    fixed formatting

--- lib/EasyPost/Event.php
+++ lib/EasyPost/Event.php
@@ -5,16 +5,16 @@ namespace EasyPost;
 class Event extends Resource
 {
 
-	public static function retrieve($rawInput=null){
-		if($rawInput == null)
-			throw new Error('The raw input must be set');
-		$values =  json_decode( $rawInput, TRUE );
-		if ($values != null) {
-			return self::constructFrom($values, get_class(), null);
-		} else {
-			throw new Error('There was a problem decoding the webhook');
-		}
-		
-	}
+    public static function retrieve($rawInput=null){
+        if($rawInput == null)
+            throw new Error('The raw input must be set');
+        $values =  json_decode( $rawInput, TRUE );
+        if ($values != null) {
+            return self::constructFrom($values, get_class(), null);
+        } else {
+            throw new Error('There was a problem decoding the webhook');
+        }
+        
+    }
 }
 ?>
