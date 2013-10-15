4cb42e4e18a0f5446d003bfda3da6e210adc2ee1
Michael Beale <michael.beale@gmail.com>
2013-10-15 11:42:14 -0600

    changed to receive

--- lib/EasyPost/Event.php
+++ lib/EasyPost/Event.php
@@ -5,7 +5,7 @@ namespace EasyPost;
 class Event extends Resource
 {
 
-    public static function retrieve($rawInput=null){
+    public static function receive($rawInput=null){
         if($rawInput == null)
             throw new Error('The raw input must be set');
         $values =  json_decode( $rawInput, TRUE );
