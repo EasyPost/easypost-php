25847b91a584797cc51aae8985ebf22c6092a824
sawyer <sawyerbateman@gmail.com>
2014-04-08 20:25:26 -0600

    Updated the CA cert bundle.

--- lib/EasyPost/EasyPost.php
+++ lib/EasyPost/EasyPost.php
@@ -7,7 +7,7 @@ abstract class EasyPost
     public static $apiKey;
     public static $apiBase = 'https://api.easypost.com/v2';
     public static $apiVersion = "2";
-    const VERSION = '2.0.8';
+    const VERSION = '2.0.9';
 
     public static function getApiKey()
     {
