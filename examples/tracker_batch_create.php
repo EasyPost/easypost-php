7649bfd6679fec0953805f69105916bdf5eae5a8
Justintime50 <jhammond@easypost.com>
2020-06-04 16:24:15 -0600

    Clarify the create_list function and example. Addresses #91

--- examples/tracker_batch_create.php
+++ examples/tracker_batch_create.php
@@ -4,16 +4,16 @@ require_once("../lib/easypost.php");
 
 \EasyPost\EasyPost::setApiKey('cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi');
 
-
 $trackers_rep = array();
 
-for ($i = 0; $i < 5; $i++) {
+for ($i = 1; $i < 6; $i++) {
     $tracker = array();
-    $tracker["tracking_code"] = "EZ2000000002";
+    $tracker["tracking_code"] = "EZ200000000$i";
     $tracker["carrier"] = "USPS";
     array_push($trackers_rep, $tracker);
 }
 
 $trackers = \EasyPost\Tracker::create_list($trackers_rep);
 
-var_dump($trackers);
+// The create_list endpoint does not return a response. You'll want to capture these
+// tracker creation events via webhook or create trackers individually to get their response.
