aa334a37993b1bd176286f1ef3adbfefe5108f35
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-01-13 15:14:45 -0700

    fix: remove dead tracking code example

--- examples/shipment_from_tracking_codes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("../lib/easypost.php");
-
-\EasyPost\EasyPost::setApiKey(getenv('API_KEY'));
-
-$shipment = \EasyPost\Shipment::create_from_tracking_code(array("tracking_code" => "1Z0X89330378901784", "options" => array("residential_to_address" => 1)));
-
-print_r($shipment->rates);
