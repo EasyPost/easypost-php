48878cbfa63e4f290770283ab344a32ba1fadcdb
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-08-22 15:26:13 -0600

    fix: lint and require test assertion

--- test/EasyPost/RequireTest.php
+++ test/EasyPost/RequireTest.php
@@ -1,7 +1,8 @@
 <?php
 
-// Manually require the library (do not use namespace) to ensure there are no import errors
-// when the autoloader is not used.
+namespace EasyPost\Test;
+
+// Manually require the library to ensure there are no import errors (eg: when the autoloader is not used)
 require 'lib/easypost.php';
 
 class RequireTest extends \PHPUnit\Framework\TestCase
@@ -9,11 +10,10 @@ class RequireTest extends \PHPUnit\Framework\TestCase
     /**
      * Tests that no errors are thrown when we import the library without using the autoloader.
      * Things like missing or extra imports should be caught by this. The actual assertion here
-     * doesn't matter, only that an import/require error isn't thrown.
+     * doesn't matter, only that an import/require error isn't thrown when the test suite runs.
      */
     public function testRequireLibrary()
     {
-        $apiBase = \EasyPost\EasyPost::getApiBase();
-        $this->assertEquals('https://api.easypost.com/v2', $apiBase);
+        $this->expectNotToPerformAssertions();
     }
 }
