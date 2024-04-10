e0e0cda186b56ffa8c6e81e61de83ac12ccfd5c4
Nate Harris <nwithan8@users.noreply.github.com>
2024-04-10 11:40:13 -0600

    [bug] Cannot determine payment method when funding/deleting (#337)
    - Use payment method object type to determine payment method type

--- lib/EasyPost/Service/BillingService.php
+++ lib/EasyPost/Service/BillingService.php
@@ -82,9 +82,10 @@ class BillingService extends BaseService
 
         if ($paymentMethodToUse != null && $paymentMethods->$paymentMethodToUse->id != null) {
             $paymentMethodId = $paymentMethods->$paymentMethodToUse->id;
-            if (strpos($paymentMethodId, 'card_') === 0) {
+            $paymentMethodObjectType = $paymentMethods->$paymentMethodToUse->object;
+            if ($paymentMethodObjectType == 'CreditCard') {
                 $endpoint = '/credit_cards';
-            } else if (strpos($paymentMethodId, 'bank_') === 0) {
+            } else if ($paymentMethodObjectType == 'BankAccount') {
                 $endpoint = '/bank_accounts';
             } else {
                 throw new PaymentException(Constants::INVALID_PAYMENT_METHOD_ERROR);
